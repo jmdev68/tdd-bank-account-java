2b1a86562f71acede48a34d9c15399dcc7fc580d
jmdev <jmdev@online.ms>
2020-08-06 12:47:27 +0200

    refactor - extract account object instantiation

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -7,37 +7,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
+    private final Account a = new Account();
+
     @Test
     public void checkNewAccountBalanceIsZero() {
-        Account a = new Account();
         assertThat(a.getBalance()).isZero();
     }
 
     @Test
     public void depositAnAmountToIncreaseTheBalance() {
-        Account b = new Account();
-        b.deposit(10);
+        a.deposit(10);
 
-        assertThat(b.getBalance()).isEqualTo(10);
+        assertThat(a.getBalance()).isEqualTo(10);
     }
 
     @Test
     public void depositMultipleAmountsToIncreaseTheBalance() {
-        Account b = new Account();
-        b.deposit(10);
-        b.deposit(20);
+        a.deposit(10);
+        a.deposit(20);
 
-        assertThat(b.getBalance()).isEqualTo(30);
+        assertThat(a.getBalance()).isEqualTo(30);
     }
 
 
     @Test
     public void depositAmountShallNotBeNegative() {
-        Account b = new Account();
-        b.deposit(-10);
+        a.deposit(-10);
 
-        assertThat(b.getBalance()).isEqualTo(0);
+        assertThat(a.getBalance()).isEqualTo(0);
     }
+
 
 
 
